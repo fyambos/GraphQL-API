7d2d463b0a2846a4b3c325f300e519270d94dc44
fyambos <feliciteyambos@gmail.com>
2024-03-12 16:01:27 +0100

    ajout data film people

--- dist/datasources/data.js
+++ dist/datasources/data.js
@@ -12,3 +12,11 @@ export const doctorsData = [
     },
 ];
 export const colorsData = ["#FF5733", "#33FF57", "#3357FF"];
+export const filmsData = [
+    { id: '1', title: 'Castle in the Sky', people: [] },
+    { id: '2', title: 'Grave of the Fireflies', people: ['1', '2'] },
+];
+export const peopleData = [
+    { id: '1', name: 'Setsuko', eyeColor: 'Brown', films: ['2'] },
+    { id: '2', name: 'Seita', eyeColor: 'Blue', films: ['2'] },
+];
