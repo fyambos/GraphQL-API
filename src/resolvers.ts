38e594428b0c014a6d87722519948560c3c0765f
fyambos <feliciteyambos@gmail.com>
2024-03-12 15:45:36 +0100

    validation des entrées dans le resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -7,14 +7,23 @@ export const resolvers = {
     Query: {
         doctors: (parent, args, context, info) => {
             const {specialities} = args
+            if (!Array.isArray(specialities) || specialities.length === 0) {
+                throw new GraphQLError('invalid specialty');
+            }
             return doctorsData.filter(doctor => specialities.includes(doctor.speciality))
           },
         doctor: (parent, args, context, info) => {
             const id = args.id
+            if (!id || typeof id !== 'string') {
+                throw new GraphQLError('invalid id');
+            }
             return doctorsData.find(d => d.id === id)
         },
         divide: (parent, args, context, info) => {
             const {number1, number2} = args
+            if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+                throw new GraphQLError('must be numbers');
+            }
             if (number2 === 0) {
               throw new GraphQLError('cannot divide by 0')
             }
@@ -22,18 +31,30 @@ export const resolvers = {
         },
         multiply: (parent, args, context, info) => {
             const { number1, number2 } = args;
+            if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+                throw new GraphQLError('must be numbers');
+            }
             return number1 * number2;
         },
         add: (parent, args, context, info) => {
             const { number1, number2 } = args;
+            if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+                throw new GraphQLError('must be numbers');
+            }
             return number1 + number2;
         },
         subtract: (parent, args, context, info) => {
             const { number1, number2 } = args;
+            if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+                throw new GraphQLError('must be numbers');
+            }
             return number1 - number2;
         },
         modulo: (parent, args, context, info) => {
             const { number1, number2 } = args;
+            if (typeof number1 !== 'number' || typeof number2 !== 'number') {
+                throw new GraphQLError('must be numbers');
+            }
             if (number2 === 0) {
               throw new GraphQLError('cannot divide by 0');
             }
@@ -41,6 +62,9 @@ export const resolvers = {
         },
         closestColor: (parent, args, context, info) => {
             const { targetColor } = args;
+            if (typeof targetColor !== 'string' || !targetColor.match(/^#[0-9A-Fa-f]{6}$/)) {
+                throw new GraphQLError('must be an hex code string, eg: "#ff0000"');
+            }
             const closestColorKey = findClosestColor(targetColor, hexColorData);
             const closestColor = hexColorData[closestColorKey];
             return closestColor;
